f6aa373b4c933546707a5ac9c52059b36693d5b6
PaulAroo <timmypaul2006@gmail.com>
2023-01-17 23:51:54 +0100

    setup: website build

---
+++ s/web/templates/index.html.ts
@@ -0,0 +1,17 @@
+
+import {html} from "xiome/x/toolbox/hamster-html/html.js"
+
+export default () => html`
+<!doctype html>
+<html>
+	<head>
+		<meta charset="utf-8"/>
+		<meta name="viewport" content="width=device-width,initial-scale=1"/>
+		<meta name="darkreader" content="dark"/>
+		<title>toolbox</title>
+	</head>
+	<body>
+		<h1>toobox</h1>
+	</body>
+<html>
+`
