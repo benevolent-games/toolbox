44deeb6c1b223a34572eddf4d59a733e2ac446b0
zenkyu <przemekgg2002@gmail.com>
2023-01-31 23:58:10 +0100

    wip: viewing modes

---
+++ s/editor-ui/viewing-modes/element.ts
@@ -0,0 +1,12 @@
+
+import {html, LitElement} from "lit"
+
+export class ViewingModes extends LitElement {
+	render() {
+		return html`
+			<div>
+				Hello is this working ?
+			</div>
+		`
+	}
+}
--- s/main.ts
+++ s/main.ts
@@ -1,10 +1,9 @@
 
 export {registerElements} from "@chasemoskal/magical"
 
-import {RangeSlider} from "./editor-ui/range-slider/element.js"
-import {BenevTheater} from "./babylon/theater/element.js"
-
+import { RangeSlider } from "./editor-ui/range-slider/element.js"
+import { ViewingModes } from "./editor-ui/viewing-modes/element.js"
 export const getElements = () => ({
 	RangeSlider,
-	BenevTheater
+	ViewingModes
 })
