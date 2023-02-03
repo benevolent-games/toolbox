222789ce3b5ce9294f98948c41153f5dff897d76
zenkyu <przemekgg2002@gmail.com>
2023-02-03 22:21:38 +0100

    add: icons with license

---
+++ s/icons/material-design-icons/fullscreen-exit.svg.ts
@@ -0,0 +1,3 @@
+import {svg} from "lit-html";
+
+export default svg`<svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24"><path fill="currentColor" d="M8 19v-3H5v-2h5v5Zm6 0v-5h5v2h-3v3Zm-9-9V8h3V5h2v5Zm9 0V5h2v3h3v2Z"/></svg>`
---
+++ s/icons/material-design-icons/fullscreen.svg.ts
@@ -0,0 +1,3 @@
+import {svg} from "lit-html";
+
+export default svg`<svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24"><path fill="currentColor" d="M5 19v-5h2v3h3v2Zm0-9V5h5v2H7v3Zm9 9v-2h3v-3h2v5Zm3-9V7h-3V5h5v5Z"/></svg>`
