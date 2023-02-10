1ac277c81725f0b376f69fa2c01e65619da2abc6
zenkyu <przemekgg2002@gmail.com>
2023-02-10 23:10:35 +0100

    add: things needed from terrarium

--- s/babylon/theater/utils/make-babylon-world.ts
+++ s/babylon/theater/utils/make-babylon-world.ts
@@ -1,6 +1,8 @@
 
 import "@babylonjs/core/Engines/Extensions/engine.query.js"
+import "@babylonjs/core/Rendering/depthRendererSceneComponent.js"
 
+import {DepthRenderer} from "@babylonjs/core/Rendering/depthRenderer.js"
 import {Scene} from "@babylonjs/core/scene.js"
 import {Engine} from "@babylonjs/core/Engines/engine.js"
 import {Color4} from "@babylonjs/core/Maths/math.color.js"
@@ -16,7 +18,8 @@ export function makeBabylonWorld(): BabylonWorld {
 		useGeometryUniqueIdsMap: true,
 		useMaterialMeshMap: true,
 	})
-
+	const renderer = new DepthRenderer(scene)
+	renderer.enabled = true
 	scene.clearColor = new Color4(62 / 255, 129 / 255, 186 / 255, 1)
 
 	const renderLoop = new Set<() => void>()
@@ -33,6 +36,8 @@ export function makeBabylonWorld(): BabylonWorld {
 	sceneInstrumentation.captureInterFrameTime = true
 	sceneInstrumentation.captureCameraRenderTime = true
 	sceneInstrumentation.captureActiveMeshesEvaluationTime = true
+	
+	; (<any>window).engine = engine
 
 	return {
 		canvas,
