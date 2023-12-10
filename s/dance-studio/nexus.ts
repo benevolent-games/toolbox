baaa2094dfc51545a2542832205d3af32378230e
Chase Moskal <chasemoskal@gmail.com>
2023-12-10 02:18:32 -0800

    remove: impulse from dance-studio

--- s/dance-studio/nexus.ts
+++ s/dance-studio/nexus.ts
@@ -2,33 +2,12 @@
 import {Nexus, Context} from "@benev/slate"
 
 import {theme} from "./theme.js"
-import {Impulse} from "../impulse/impulse.js"
 import {World} from "./models/world/world.js"
 import {Loader} from "./models/loader/loader.js"
-import {Stick} from "../impulse/nubs/stick/device.js"
 
 export const nexus = new Nexus(new class extends Context {
 	theme = theme
 	world = new World()
 	loader = new Loader(this.world.scene)
-
-	sticks = {
-		movement: new Stick("movement"),
-	}
-
-	impulse = new Impulse({
-		binds: {
-			normal: {
-				buttons: {},
-				vectors: {movement: ["movement"]},
-			},
-		},
-		devices: [
-			this.sticks.movement,
-		],
-		modes: [
-			"normal",
-		],
-	})
 })
 
