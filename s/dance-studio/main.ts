fb4c880a4b1417acf47e92cc9b0e263908defae3
Chase Moskal <chasemoskal@gmail.com>
2023-12-10 17:34:29 -0800

    sketch: improved download facilities

--- s/dance-studio/main.ts
+++ s/dance-studio/main.ts
@@ -6,5 +6,6 @@ import {DanceStudio} from "./dom/elements/studio/element.js"
 
 register_to_dom({DanceStudio})
 
-nexus.context.loader.ingest_glb_from_url("https://filebin.net/6htk8w8pu62ac3dd/bungledanimations10.glb")
+// nexus.context.loader.ingest_glb_from_url("https://filebin.net/6htk8w8pu62ac3dd/bungledanimations10.glb")
+// // nexus.context.loader.ingest_but_actually_fail()
 
--- s/dance-studio/models/loader/loader.ts
+++ s/dance-studio/models/loader/loader.ts
@@ -1,45 +1,77 @@
 
-import {Op, signals} from "@benev/slate"
+import {Op, explode_promise, signals} from "@benev/slate"
 import {Scene} from "@babylonjs/core/scene.js"
 import {SceneLoader} from "@babylonjs/core/Loading/sceneLoader.js"
 
 import {Glb} from "./utils/types.js"
 import {Choreographer} from "./choreographer/choreographer.js"
+import { nap } from "@benev/turtle"
 
 export class Loader {
 	#scene: Scene
+
 	readonly glb = signals.op<Glb | null>(Op.ready(null))
+	#abort: null | (() => Promise<void>) = null
 
 	constructor(scene: Scene) {
 		this.#scene = scene
 	}
 
-	unload_glb() {
+	async unload_glb() {
 		const glb = this.glb.payload
 		if (glb) {
 			glb.container.removeAllFromScene()
 			glb.container.dispose()
 			this.glb.setReady(null)
 		}
+		else if (this.#abort) {
+			await this.#abort()
+		}
 	}
 
 	async ingest(file: File) {
-		if (this.glb.isLoading())
-			return
+		await this.unload_glb()
+		await this.glb.load(() => this.#load_glb_asset_container(fileData(file)))
+	}
 
-		this.unload_glb()
-		await this.glb.run(() => this.#load_glb_asset_container(fileData(file)))
+	async ingest_but_actually_fail() {
+		await this.unload_glb()
+		await this.glb.load(async() => {
+			const {promise, resolve} = explode_promise<FileData | null>()
+			const abort = () => resolve(null)
+			this.#abort = async() => {
+				abort()
+				return new Promise(resolve => this.glb.once(() => resolve()))
+			}
+			try {
+				const file = await promise
+				return file
+					? await this.#load_glb_asset_container(file)
+					: null
+			}
+			finally {
+				this.#abort = null
+			}
+		})
 	}
 
 	async ingest_glb_from_url(url: string) {
-		if (this.glb.isLoading())
-			return
-
-		this.unload_glb()
-
-		await this.glb.run(async() => {
-			const file = await download(url)
-			return await this.#load_glb_asset_container(file)
+		await this.unload_glb()
+		await this.glb.load(async() => {
+			const {promise, abort} = download(url)
+			this.#abort = async() => {
+				abort()
+				return new Promise(resolve => this.glb.once(() => resolve()))
+			}
+			try {
+				const file = await promise
+				return file
+					? await this.#load_glb_asset_container(file)
+					: null
+			}
+			finally {
+				this.#abort = null
+			}
 		})
 	}
 
@@ -70,27 +102,55 @@ export type FileData = {
 	objectUrl: string
 	filename: string
 	filesize: number
+	revoke: () => void
+}
+
+export type Download = {
+	abort: () => void
+	promise: Promise<FileData | null>
 }
 
 function fileData(file: File): FileData {
+	const objectUrl = URL.createObjectURL(file)
+	const revoke = () => URL.revokeObjectURL(objectUrl)
 	return {
-		objectUrl: URL.createObjectURL(file),
+		objectUrl,
+		revoke,
 		filename: file.name,
 		filesize: file.size,
 	}
 }
 
-async function download(url: string): Promise<FileData> {
-	const response = await fetch(url)
-
-	if (!response.ok)
-		throw new Error(`failed ${response.statusText}`)
-
-	const blob = await response.blob()
-	const objectUrl = URL.createObjectURL(blob)
-	const filesize = blob.size
+function download(url: string): Download {
+	const abortController = new AbortController()
+	const abort = () => abortController.abort()
 	const filename = new URL(url).pathname.split("/").at(-1) ?? "unknown"
 
-	return {objectUrl, filename, filesize}
+	const promise = fetch(url, {signal: abortController.signal})
+		.then(response => {
+			if (!response.ok)
+				throw new Error(`failed to load "${filename}"`)
+			return response
+		})
+		.then(response => response.blob())
+		.then(blob => {
+			const objectUrl = URL.createObjectURL(blob)
+			const revoke = () => URL.revokeObjectURL(objectUrl)
+			const data: FileData = {
+				objectUrl,
+				revoke,
+				filename,
+				filesize: blob.size,
+			}
+			return data
+		})
+		.catch(error => {
+			if (error.name === "AbortError")
+				return null
+			else
+				throw error
+		})
+
+	return {abort, promise}
 }
 
