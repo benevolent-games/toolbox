6f112958f49cd755bc93f208b7fb25d09aa4e79f
Chase Moskal <chasemoskal@gmail.com>
2023-12-10 17:58:30 -0800

    update: to newer animations

--- s/dance-studio/models/loader/choreographer/choreographer.ts
+++ s/dance-studio/models/loader/choreographer/choreographer.ts
@@ -9,42 +9,42 @@ import {scalar} from "../../../../tools/math/scalar.js"
 import {activate_animations} from "./utils/active_animations.js"
 import {process_animation_groups} from "./utils/process_animation_groups.js"
 
-function configure_animation(anims: AnimLibrary) {
-	return activate_animations(anims, [
-		["idle", "weighted_looper"],
-
-		["legs_running", "weighted_looper"],
-		["legs_strafeleft", "weighted_looper"],
-		["legs_straferight", "weighted_looper"],
-		["runningbackwards", "weighted_looper"],
-
-		["arms_running", "weighted_looper"],
-		["arms_strafeleft", "weighted_looper"],
-		["arms_straferight", "weighted_looper"],
-	])
-}
-
 // function configure_animation(anims: AnimLibrary) {
 // 	return activate_animations(anims, [
-// 		["spine", "primary"],
+// 		["idle", "weighted_looper"],
 
-// 		["legs_stand_swivel", "primary"],
-// 		["legs_stand_adjust_left", "weighted_looper"],
-// 		["legs_stand_adjust_right", "weighted_looper"],
+// 		["legs_running", "weighted_looper"],
+// 		["legs_strafeleft", "weighted_looper"],
+// 		["legs_straferight", "weighted_looper"],
+// 		["runningbackwards", "weighted_looper"],
 
-// 		["legs_stand_stationary", "primary"],
-// 		["legs_stand_forward", "weighted_looper"],
-// 		["legs_stand_backward", "weighted_looper"],
-// 		["legs_stand_leftward", "weighted_looper"],
-// 		["legs_stand_rightward", "weighted_looper"],
-
-// 		["arms_stand_unequipped_stationary", "primary"],
-// 		["arms_stand_unequipped_forward", "weighted_looper"],
-// 		["arms_stand_unequipped_backward", "weighted_looper"],
-// 		["arms_stand_unequipped_leftward", "weighted_looper"],
-// 		["arms_stand_unequipped_rightward", "weighted_looper"],
+// 		["arms_running", "weighted_looper"],
+// 		["arms_strafeleft", "weighted_looper"],
+// 		["arms_straferight", "weighted_looper"],
 // 	])
 // }
+
+function configure_animation(anims: AnimLibrary) {
+	return activate_animations(anims, [
+		["spine", "primary"],
+
+		["swivel", "primary"],
+		["legs_stand_adjust_left", "weighted_looper"],
+		["legs_stand_adjust_right", "weighted_looper"],
+
+		["legs_stand_stationary", "weighted_looper"],
+		["legs_stand_forward", "weighted_looper"],
+		["legs_stand_backward", "weighted_looper"],
+		["legs_stand_leftward", "weighted_looper"],
+		["legs_stand_rightward", "weighted_looper"],
+
+		["arms_stand_unequipped_stationary", "weighted_looper"],
+		["arms_stand_unequipped_forward", "weighted_looper"],
+		["arms_stand_unequipped_backward", "weighted_looper"],
+		["arms_stand_unequipped_leftward", "weighted_looper"],
+		["arms_stand_unequipped_rightward", "weighted_looper"],
+	])
+}
 
 export class Choreographer {
 	all_animations: Pojo<AnimationGroup>
@@ -65,42 +65,53 @@ export class Choreographer {
 		const ambulate = this.#ambulation_weights.update(inputs.ambulate)
 
 		{
-			const idleness = scalar.cap(1 - vec2.magnitude(ambulate), 0, 1)
+			const stillness = scalar.cap(1 - vec2.magnitude(ambulate), 0, 1)
 			const [x, y] = ambulate
 			const w = scalar.cap(y, 0, 1)
 			const a = -scalar.cap(x, -1, 0)
 			const s = -scalar.cap(y, -1, 0)
 			const d = scalar.cap(x, 0, 1)
 
+			// spine and swivel
 			this.#apply_weights(
-				[anims.idle, idleness],
-
-				[anims.legs_running, w],
-				[anims.arms_running, w],
-
-				[anims.runningbackwards, s],
-
-				[anims.legs_strafeleft, a],
-				[anims.arms_strafeleft, a],
-
-				[anims.legs_straferight, d],
-				[anims.arms_straferight, d],
+				[anims.spine, 1.0],
+				[anims.swivel, 1.0],
 			)
+
+			// legs
+			this.#apply_weights(
+				[anims.legs_stand_stationary, stillness],
+				[anims.legs_stand_forward, w],
+				[anims.legs_stand_backward, s],
+				[anims.legs_stand_leftward, a],
+				[anims.legs_stand_rightward, d],
+			)
+
+			// arms
+			this.#apply_weights(
+				[anims.arms_stand_unequipped_stationary, stillness],
+				[anims.arms_stand_unequipped_forward, w],
+				[anims.arms_stand_unequipped_backward, s],
+				[anims.arms_stand_unequipped_leftward, a],
+				[anims.arms_stand_unequipped_rightward, d],
+			)
+
+			// this.#apply_weights(
+			// 	[anims.idle, idleness],
+
+			// 	[anims.legs_running, w],
+			// 	[anims.arms_running, w],
+
+			// 	[anims.runningbackwards, s],
+
+			// 	[anims.legs_strafeleft, a],
+			// 	[anims.arms_strafeleft, a],
+
+			// 	[anims.legs_straferight, d],
+			// 	[anims.arms_straferight, d],
+			// )
 		}
 
-		// this.#apply_weights(
-		// 	[anims.legs_stand_forward, w],
-		// 	[anims.arms_stand_unequipped_forward, w],
-
-		// 	[anims.legs_stand_backward, s],
-		// 	[anims.arms_stand_unequipped_backward, s],
-
-		// 	[anims.legs_stand_leftward, a],
-		// 	[anims.arms_stand_unequipped_leftward, a],
-
-		// 	[anims.legs_stand_rightward, d],
-		// 	[anims.arms_stand_unequipped_rightward, d],
-		// )
 	}
 
 	#apply_weights(
