651ff4e6f422f3a0e8d24e41aeff1f018dd54500
Chase Moskal <chasemoskal@gmail.com>
2023-12-13 02:51:56 -0800

    fix: dance studio

--- s/dance-studio/main.ts
+++ s/dance-studio/main.ts
@@ -1,4 +1,10 @@
 
+import "@babylonjs/loaders/glTF/index.js"
+import "@babylonjs/core/Engines/index.js"
+import "@babylonjs/core/Culling/ray.js"
+import "@babylonjs/core/Rendering/edgesRenderer.js"
+import "@babylonjs/core/Animations/index.js"
+
 import {register_to_dom} from "@benev/slate"
 
 import {nexus} from "./nexus.js"
