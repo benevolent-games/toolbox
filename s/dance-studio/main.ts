07e0324e7f80f00511e229f60b0b335a85571010
Chase Moskal <chasemoskal@gmail.com>
2023-12-31 07:07:25 -0800

    use: temp filebin url

--- s/dance-studio/main.ts
+++ s/dance-studio/main.ts
@@ -15,5 +15,6 @@ register_to_dom({DanceStudio})
 ;(window as any).nexus = nexus
 
 // nexus.context.loader.ingest_glb_from_url("/temp/bungledanimations18.glb")
-await nexus.context.loader.ingest_glb_from_url("/temp/knightanimations9.glb")
+// await nexus.context.loader.ingest_glb_from_url("/temp/knightanimations9.glb")
+await nexus.context.loader.ingest_glb_from_url("https://filebin.net/o9g5lbh4ex6frwrf/knightanimations9.glb")
 
