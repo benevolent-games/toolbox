d6d5faa7cb0b647cb3ace252b09484c297ad5f75
Chase Moskal <chasemoskal@gmail.com>
2024-01-03 07:19:06 -0800

    update: asset cloud links

--- s/dance-studio/main.ts
+++ s/dance-studio/main.ts
@@ -14,8 +14,6 @@ register_to_dom({DanceStudio})
 
 ;(window as any).nexus = nexus
 
-// nexus.context.loader.ingest_glb_from_url("/temp/bungledanimations18.glb")
-// await nexus.context.loader.ingest_glb_from_url("https://filebin.net/o9g5lbh4ex6frwrf/knightanimations12.glb")
-
-await nexus.context.loader.ingest_glb_from_url("/temp/knightanimations14.glb")
+// await nexus.context.loader.ingest_glb_from_url("/temp/knightanimations14.glb")
+await nexus.context.loader.ingest_glb_from_url("https://filebin.net/wn3s9gkxikqibf50/knightanimations14.glb")
 
