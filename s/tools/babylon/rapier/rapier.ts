ccb0917e8694aedb21a1d761d93acdb16ebd5e74
Chase Moskal <chasemoskal@gmail.com>
2024-01-04 14:45:17 -0800

    sketch: further babylon rapier integration

--- s/tools/babylon/rapier/rapier.ts
+++ s/tools/babylon/rapier/rapier.ts
@@ -10,6 +10,8 @@ import {PBRMaterial} from "@babylonjs/core/Materials/PBR/pbrMaterial.js"
 import {quat} from "../../math/quat.js"
 import {Vec3, vec3} from "../../math/vec3.js"
 import {babylonian} from "../../math/babylonian.js"
+import {Mesh} from "@babylonjs/core/Meshes/mesh.js"
+import {VertexData} from "@babylonjs/core/Meshes/mesh.vertexData.js"
 
 export type Container<X> = {
 	value: X
@@ -27,8 +29,8 @@ export type Body = {
 export class PhysicsIntegration {
 	#scene: Scene
 	#world: Rapier.World
-	#bodies = new Set<Body>()
 	#counter = 0
+	#synchronizedBodies = new Set<Body>()
 
 	constructor({scene, gravity, timestep}: {
 			scene: Scene,
@@ -68,18 +70,18 @@ export class PhysicsIntegration {
 	}
 
 	body(
+			transform: TransformNode,
 			rigidDesc: Rapier.RigidBodyDesc,
 			colliderDesc: Rapier.ColliderDesc,
 		) {
-		const transform = this.#new_transform_node()
 		const rigid = this.#world.createRigidBody(rigidDesc)
 		const collider = this.#world.createCollider(colliderDesc, rigid)
 		const body: Body = {transform, rigid, collider}
-		this.#bodies.add({transform, rigid, collider})
+		this.#synchronizedBodies.add({transform, rigid, collider})
 		return asContainer({
 			value: body,
 			dispose: () => {
-				this.#bodies.delete(body)
+				this.#synchronizedBodies.delete(body)
 				this.#world.removeCollider(collider, false)
 				this.#world.removeRigidBody(rigid)
 			},
@@ -101,8 +103,8 @@ export class PhysicsIntegration {
 			.cuboid(...vec3.divideBy(spec.dimensions, 2))
 			.setDensity(spec.density)
 
-		const container = this.body(rigid, collider)
-		const {transform} = container.value
+		const transform = this.#new_transform_node()
+		const container = this.body(transform, rigid, collider)
 		const [width, height, depth] = spec.dimensions
 
 		const mesh = MeshBuilder.CreateBox(
@@ -112,6 +114,7 @@ export class PhysicsIntegration {
 		)
 
 		mesh.material = spec.material ?? this.colors.red
+		mesh.setParent(transform)
 
 		return asContainer({
 			value: {transform, rigid, collider, mesh},
@@ -124,11 +127,13 @@ export class PhysicsIntegration {
 
 	character(size: {halfHeight: number, radius: number}) {
 		const controller = this.#world.createCharacterController(0.01)
+		const transform = this.#new_transform_node()
 		const container = this.body(
+			transform,
 			Rapier.RigidBodyDesc.kinematicPositionBased(),
 			Rapier.ColliderDesc.capsule(size.halfHeight, size.radius),
 		)
-		const {transform, rigid, collider} = container.value
+		const {rigid, collider} = container.value
 		const applyMovement = (velocity: Vec3) => {
 			controller.computeColliderMovement(
 				collider,
@@ -153,6 +158,7 @@ export class PhysicsIntegration {
 		)
 
 		mesh.material = this.colors.cyan
+		mesh.setParent(transform)
 
 		return asContainer({
 			value: {transform, rigid, collider, mesh, applyMovement},
@@ -163,11 +169,35 @@ export class PhysicsIntegration {
 		})
 	}
 
+	static_trimesh(mesh: Mesh) {
+		const {positions, indices} = VertexData.ExtractFromMesh(mesh)
+
+		const rigid = this.#world.createRigidBody(
+			Rapier.RigidBodyDesc.fixed()
+		)
+
+		const collider = this.#world.createCollider(
+			Rapier.ColliderDesc.trimesh(
+				new Float32Array(positions!),
+				new Uint32Array(indices!),
+			),
+			rigid,
+		)
+
+		return asContainer({
+			value: {rigid, collider},
+			dispose: () => {
+				this.#world.removeCollider(collider, false)
+				this.#world.removeRigidBody(rigid)
+			},
+		})
+	}
+
 	step() {
 		this.#world.step()
 
 		// update babylon positions and rotations
-		for (const {transform, rigid} of this.#bodies) {
+		for (const {transform, rigid} of this.#synchronizedBodies) {
 			transform.position.set(...vec3.from.xyz(rigid.translation()))
 
 			if (transform.rotationQuaternion)
