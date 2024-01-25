74d2adf70a36a8ac6649b74b13f705d8595c883c
Chase Moskal <chasemoskal@gmail.com>
2024-01-25 11:06:24 -0800

    ecs4: re-export in index

--- s/index.ts
+++ s/index.ts
@@ -6,6 +6,7 @@ export * from "./common/porthole/porthole.js"
 export * from "./common/theme.js"
 
 export * from "./ecs/ecs3.js"
+export * from "./ecs/ecs4.js"
 
 export * from "./impulse/devices/keyboard.js"
 export * from "./impulse/devices/pointer_buttons.js"
