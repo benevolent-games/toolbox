a92364b0d6f5da490b7e30da28463078c317c49c
Chase Moskal <chasemoskal@gmail.com>
2024-02-21 00:03:48 -0800

    pack: bulk processing of glbs

--- s/routines/pack.ts
+++ s/routines/pack.ts
@@ -1,30 +1,38 @@
 #!/usr/bin/env node
 
-import {join} from "path"
+import {glob} from "glob"
 import {dedup} from "@gltf-transform/functions"
+import {join, relative, resolve, parse} from "path"
 
 import {args} from "./parts/args.js"
 import {tiers} from "./parts/tiers.js"
 import {glb_io} from "./parts/glb_io.js"
 import {log_glb} from "./parts/log_glb.js"
 
-const {inpath, outdir, verbose} = args()
-const gio = await glb_io()
-const original = await gio.read(inpath)
+const {indir, outdir, verbose} = args()
+const inpattern = `${indir}/**/*.glb`
 
-log_glb(original)
-await original.document.transform(dedup())
+for (const inpath of await glob(inpattern, {nodir: true})) {
+	const {dir, name} = parse(relative(resolve(indir), inpath))
+	const outpath = (index: number) => join(outdir, `${dir}/${name}.${index}.glb`)
 
-for (const [index, [,transforms]] of tiers.entries()) {
-	const document = original.document.clone()
-	await document.transform(...transforms)
-	const report = await gio.write(join(outdir, `pack.${index}.glb`), document)
+	const gio = await glb_io()
+	const original = await gio.read(inpath)
 
-	log_glb(report)
+	log_glb(original)
+	await original.document.transform(dedup())
 
-	if (!!verbose && index === 0) {
-		for (const node of document.getRoot().listNodes()) {
-			console.log(` - ${node.getName()}`)
+	for (const [index, [,transforms]] of tiers.entries()) {
+		const document = original.document.clone()
+		await document.transform(...transforms)
+		const report = await gio.write(outpath(index), document)
+
+		log_glb(report)
+
+		if (!!verbose && index === 0) {
+			for (const node of document.getRoot().listNodes()) {
+				console.log(` - ${node.getName()}`)
+			}
 		}
 	}
 }
--- s/routines/parts/args.ts
+++ s/routines/parts/args.ts
@@ -1,13 +1,13 @@
 
 export function args() {
-	const [,, inpath, outdir, verbose] = process.argv
+	const [,, indir, outdir, verbose] = process.argv
 
-	if (!inpath)
-		throw new Error("inpath parameter missing")
+	if (!indir)
+		throw new Error("indir parameter missing")
 
 	if (!outdir)
 		throw new Error("outdir parameter missing")
 
-	return {inpath, outdir, verbose}
+	return {indir, outdir, verbose}
 }
 
