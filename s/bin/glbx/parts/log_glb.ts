2147772de24e28c253a1a61d82b37755a0dcd87f
Chase Moskal <chasemoskal@gmail.com>
2024-02-21 00:09:27 -0800

    bin: reorganize/rename scripts, move into bin

--- s/bin/glbx/parts/log_glb.ts
+++ s/bin/glbx/parts/log_glb.ts
@@ -1,6 +1,6 @@
 
 import {basename} from "path"
-import {human} from "../../tools/human.js"
+import {human} from "../../../tools/human.js"
 
 export function log_glb({path, binary}: {path: string, binary: Uint8Array}) {
 	console.log(basename(path), "::", human.bytes(binary.byteLength))
