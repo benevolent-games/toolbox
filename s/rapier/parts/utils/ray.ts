9c58835c043bfb2852bd153810432374085e768a
Chase Moskal <chasemoskal@gmail.com>
2024-03-08 13:39:49 -0800

    physics: add ray helper

---
+++ s/rapier/parts/utils/ray.ts
@@ -0,0 +1,11 @@
+
+import {Rapier} from "../../rapier.js"
+import {Vec3, vec3} from "../../../math/exports.js"
+
+export function ray(position: Vec3, direction: Vec3) {
+	return new Rapier.Ray(
+		vec3.to.xyz(position),
+		vec3.to.xyz(direction),
+	)
+}
+
--- s/rapier/physics.ts
+++ s/rapier/physics.ts
@@ -4,6 +4,7 @@ import {Scene} from "@babylonjs/core/scene.js"
 import {Rapier} from "./rapier.js"
 import {Vec3} from "../math/vec3.js"
 import {vec3} from "../math/exports.js"
+import {ray} from "./parts/utils/ray.js"
 import {Grouping} from "./parts/grouping.js"
 import * as prefabs from "./parts/prefabs.js"
 import {PhysicsBonding} from "./parts/bonding.js"
@@ -11,6 +12,7 @@ import {prefabulate} from "./parts/utils/prefab.js"
 
 export class Physics {
 	static readonly grouping = Grouping
+	static readonly ray = ray
 	readonly scene: Scene
 	readonly world: Rapier.World
 	readonly bonding: PhysicsBonding
