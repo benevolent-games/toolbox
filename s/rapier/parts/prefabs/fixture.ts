baef0aba934a102a08c595d1481735eb87567668
Chase Moskal <chasemoskal@gmail.com>
2024-03-08 13:40:07 -0800

    physics: improve fixture

--- s/rapier/parts/prefabs/fixture.ts
+++ s/rapier/parts/prefabs/fixture.ts
@@ -1,14 +1,20 @@
 
+import {Material} from "@babylonjs/core/Materials/material.js"
 import {MeshBuilder} from "@babylonjs/core/Meshes/meshBuilder.js"
 
 import {Rapier} from "../../rapier.js"
 import {prefab} from "../utils/prefab.js"
+import {Vec3} from "../../../math/vec3.js"
+import {vec3} from "../../../math/exports.js"
 import {label} from "../../../tools/label.js"
 import {Trashcan} from "../../../tools/trashcan.js"
+import { applyMaterial } from "../utils/apply_material.js"
 
 export const fixture = prefab(physics => (o: {
-		radius: number
-		subdivisions: number
+		position: Vec3
+		material: Material | null
+		radius?: number
+		subdivisions?: number
 	}) => {
 
 	const {bag, dispose} = new Trashcan()
@@ -16,7 +22,7 @@ export const fixture = prefab(physics => (o: {
 	const mimic = bag(
 		MeshBuilder.CreateIcoSphere(
 			label("fixture-sphere"),
-			{radius: o.radius, subdivisions: o.subdivisions},
+			{radius: o.radius ?? 0.2, subdivisions: o.subdivisions ?? 2},
 			physics.scene,
 		)
 	).dump(m => m.dispose())
@@ -26,6 +32,10 @@ export const fixture = prefab(physics => (o: {
 			Rapier.RigidBodyDesc.fixed()
 		)
 	).dump(r => physics.world.removeRigidBody(r))
+
+	applyMaterial(mimic, o.material)
+	mimic.position.set(...o.position)
+	rigid.setTranslation(vec3.to.xyz(o.position), true)
 
 	return {rigid, mimic, dispose}
 })
