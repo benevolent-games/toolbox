b92250cef77fee1527e73464fa8bfd514a65770f
Chase Moskal <chasemoskal@gmail.com>
2024-03-10 18:06:45 -0700

    pubsub: add clear function

--- s/tools/pubsub.ts
+++ s/tools/pubsub.ts
@@ -1,23 +1,40 @@
 
+export interface Pubsub<P extends any[]> {
+	(fn: (...p: P) => void): () => void
+	publish(...p: P): void
+	clear(): void
+}
+
 /**
  * simple pub-sub mechanism.
  *
- *     const onWhatever = pubb((a: number, b: boolean) => console.log(a, b))
- *     onWhatever.publish(123, true)
+ *     // create pubsub function
+ *     const onCount = pubsub<[string, number]>()
+ *
+ *     // subscribe
+ *     const stop = onCount((a, b) => console.log(a, b))
+ *
+ *     // publish
+ *     onWhatever.publish("count", 123)
+ *
+ *     // unsubscribe
+ *     stop()
  *
  */
-export function pubsub<P extends any[]>() {
+export function pubsub<P extends any[]>(): Pubsub<P> {
 	const set = new Set<(...p: P) => void>()
 
 	function subscribe(fn: (...p: P) => void) {
 		set.add(fn)
-		return () => set.delete(fn)
+		return () => { set.delete(fn) }
 	}
 
 	subscribe.publish = (...p: P) => {
 		for (const fn of set)
 			fn(...p)
 	}
+
+	subscribe.clear = () => set.clear()
 
 	return subscribe
 }
