96c1e565058a356c5fd40e781e6a5ef4c628da57
Chase Moskal <chasemoskal@gmail.com>
2024-03-29 16:00:43 -0700

    add: archetype 'add' method

--- s/ecs7/parts/archetype.ts
+++ s/ecs7/parts/archetype.ts
@@ -14,8 +14,15 @@ export class Archetype<Sel extends Selector> implements Arch<Sel> {
 
 	extend<Sel2 extends Selector>(selector: Sel2, state: State<Sel2>) {
 		return new Archetype<Sel2 & Sel>(
-			{...this.selector, ...selector} as Sel2 & Sel,
+			{...this.selector, ...selector},
 			{...this.state, ...state} as State<Sel2 & Sel>,
+		)
+	}
+
+	add<Sel2 extends Selector>(archetype: Archetype<Sel2>) {
+		return new Archetype<Sel2 & Sel>(
+			{...this.selector, ...archetype.selector},
+			{...this.state, ...archetype.state} as State<Sel2 & Sel>,
 		)
 	}
 }
