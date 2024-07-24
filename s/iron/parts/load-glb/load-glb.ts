a10829f023512fd88bb2ccb5bd8966239a1556ee
Chase Moskal <chasemoskal@gmail.com>
2024-07-23 23:02:39 -0700

    remove: obsolete emission fix from loadGlb

--- s/iron/parts/load-glb/load-glb.ts
+++ s/iron/parts/load-glb/load-glb.ts
@@ -3,9 +3,11 @@ import {Scene} from "@babylonjs/core/scene.js"
 import {SceneLoader} from "@babylonjs/core/Loading/sceneLoader.js"
 
 import {fix_animation_groups} from "./fix_animation_groups.js"
-import {fix_emissive_colors_by_converting_them_to_gamma_space} from "./fix_emissive_colors_by_converting_them_to_gamma_space.js"
 
-export async function loadGlb(scene: Scene, url: string) {
+/**
+ * if exporting from blender, use "standard" lighting mode (instead of "unitless" or "raw")
+ */
+export async function loadGlb(scene: Scene, url: string, applyFixes = true) {
 	const container = await SceneLoader.LoadAssetContainerAsync(
 		url,
 		undefined,
@@ -16,8 +18,8 @@ export async function loadGlb(scene: Scene, url: string) {
 
 	container.removeAllFromScene()
 
-	fix_animation_groups(container.animationGroups)
-	fix_emissive_colors_by_converting_them_to_gamma_space(container.materials)
+	if (applyFixes)
+		fix_animation_groups(container.animationGroups)
 
 	return container
 }
